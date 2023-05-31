b2a3b86b5e9b53688a513649573bc8237cfd83b7
WizardMode <ubik203@gmail.com>
2023-05-30 21:22:13 -0700

    padding

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,6 @@
 function Page(props) {
     return (
-        <div>
+        <div className="gif-section">
             <h2 className="gif-title">{props.title}</h2>
             <div className={`gif-img ${props.theme}`} id={props.gif}>
             </div>
