9532c24c70b9ebfebe2aceef669da3f7f45184a6
WizardMode <ubik203@gmail.com>
2023-05-30 17:43:05 -0700

    map

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
 import Header from "./components/Header";
 import Page from "./components/Page";
 import Footer from "./components/Footer";
+import data from "./data";
 
 function App() {
+
+  const pageElements = data.map(item => (
+      <Page
+        key={item.id}
+        title={item.title}
+        theme={item.theme}
+        gif={item.gif}
+      />
+  ))
+
   return (
-    <div>
+    <div className="main">
       <Header />
       <h3 className="book-hint">Hint: tap the page over</h3>
-      <Page 
-        title="That was fun 🎉"
-        theme="rat"
-        gif="gify-rat"
-      />
-      <Page
-        title="How happy I'm for you 😻"
-        theme="lizard"
-        gif="gify-lizard"
-      />
-      <Page
-        title="Give all you got 💪"
-        theme="rooster"
-        gif="gify-rooster"
-      />
-      <Page
-        title="Enjoy the ride 🕹️"
-        theme="penguin"
-        gif="gify-penguin"
-      />
-      <Page
-        title="For reals 🧚"
-        theme="frog"
-        gif="gify-frog"
-      />
-      <Page
-        title="You got this ⚡"
-        theme="whale"
-        gif="gify-whale"
-      />
-      <Page
-        title="Always 👑"
-        theme="lady"
-        gif="gify-lady"
-      />
+      {pageElements}
       <Footer />
     </div>
   )
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default [
+    {
+        id: 1,
+        title: "That was fun 🎉",
+        theme: "rat",
+        gif: "gif-1"
+    },
+    {
+        id: 2,
+        title: "How happy I'm for you 😻",
+        theme: "lizard",
+        gif: "gif-2"
+    },
+    {
+        id: 3,
+        title: "Give all you got 💪",
+        theme: "rooster",
+        gif: "gif-3"
+    },
+    {
+        id: 4,
+        title: "Enjoy the ride 🕹️",
+        theme: "penguin",
+        gif: "gif-4"
+    },
+    {
+        id: 5,
+        title: "For reals 🧚",
+        theme: "frog",
+        gif: "gif-5"
+    },
+    {
+        id: 6,
+        title: "You got this ⚡",
+        theme: "whale",
+        gif: "gif-6"
+    },
+    {
+        id: 7,
+        title: "Always 👑",
+        theme: "lady",
+        gif: "gif-7"
+    }
+
+]
